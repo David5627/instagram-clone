95b711759e436a14992d102483e3b48a2596b275
David Kilonzo <binginaya708@gmail.com>
2021-01-17 22:56:34 +0300

    modified base.html

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    url(r'', include('people.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
+    # path('auth/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+ 
 ]
