10b9e3973a6cb7da9f4a2541d86ada188a2b3e53
David Kilonzo <binginaya708@gmail.com>
2021-01-18 01:05:20 +0300

    change database from prod to dev

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -84,21 +84,21 @@ WSGI_APPLICATION = 'Instagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':'instagram',
-#         'USER':'moringa',
-#         'PASSWORD':'Access',
-#     }
-# }
-
-
 DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'instagram',
+        'USER':'moringa',
+        'PASSWORD':'Access',
+    }
+}
+
+
+# DATABASES = {
+#        'default': dj_database_url.config(
+#            default=config('DATABASE_URL')
+#        )
+#    }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
