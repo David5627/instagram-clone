b0d0445a4ae53b1df703092110e861d74e60f5d5
David Kilonzo <binginaya708@gmail.com>
2021-01-17 23:09:35 +0300

    created class test for profiles

--- people/tests.py
+++ people/tests.py
@@ -43,3 +43,50 @@ class TestImage(TestCase):
         User.objects.all().delete()
         Profile.objects.all().delete()
         Image.objects.all().delete()
+
+
+class TestProfile(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username = "username", email = "e@e.com", password = "password")
+        self.profile= Profile(bio = "new me", user = self.user)
+        # self.img =  Image(name ="test", caption = "haha", likes = 0, profile = self.profile)
+       
+    def test_instance(self):
+        self.user.save()
+        self.profile.save()
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_save_profile(self):
+        self.user.save()
+        self.profile.save()
+        profs = Profile.objects.all()
+        self.assertTrue(len(profs) > 0)
+
+    def test_search_users(self):
+        self.user.save()
+        self.profile.save()
+        user = Profile.search_users("username")
+        self.assertTrue(len(user) > 0)
+
+    def test_delete_profile(self):
+        self.user.save()
+        self.profile.save()
+        self.profile.delete_profile()
+        profs = Profile.objects.all()
+        self.assertTrue(len(profs) == 0)
+
+    def test_update_profile(self):
+        cwd = os.getcwd()
+        self.user.save()
+        self.profile.save()
+        self.assertTrue(self.profile.dp == None)
+        self.profile.update_profile(f'{cwd}/media/home.png',"changed")
+        profs = Profile.objects.filter(bio = "changed")
+        self.assertTrue(len(profs) > 0)
+
+    def tearDown(self):
+        User.objects.all().delete()
+        self.assertTrue(len(User.objects.all()) == 0)
+        Profile.objects.all().delete()
+        self.assertTrue(len(Profile.objects.all()) == 0)
