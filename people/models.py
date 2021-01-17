0f900df3c88b6169c92246cd947d692d62c43da8
David Kilonzo <binginaya708@gmail.com>
2021-01-17 22:46:37 +0300

    created class profile

--- people/models.py
+++ people/models.py
@@ -17,3 +17,27 @@ class InstaPhotos(models.Model):
 class Comment(models.Model):
     comment = models.CharField(max_length = 300)
 
+
+class Profile(models.Model):
+    bio = models.TextField(blank = True, null = True)
+    dp = CloudinaryField('image', null = True, blank = True)
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    following = models.ManyToManyField(Following)
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+        Profile.objects.filter(id = self.id).delete()
+    def update_profile(self, dp = None, bio = None):
+        if dp is not None:
+            Profile.objects.filter(id = self.id).update(dp = cloudinary.uploader.upload_resource(dp))
+        if bio is not None:
+            Profile.objects.filter(id = self.id).update(bio = bio)
+
+    @classmethod
+    def search_users(cls, search_term):
+        users = [user for user in User.objects.all()]
+        user = User.objects.filter(username__icontains = search_term).all()
+        print(users)
+        return user
+
+
