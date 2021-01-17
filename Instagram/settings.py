68f412a1f0f8365ceeaa9073ed2802a3341f3f43
David Kilonzo <binginaya708@gmail.com>
2021-01-17 22:15:16 +0300

    initial commit

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -84,26 +84,15 @@ WSGI_APPLICATION = 'Instagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':'instagram',
-#         'USER':'moringa',
-#         'PASSWORD':'Access',
-#     }
-# }
-
-
 DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'instagram',
+        'USER':'moringa',
+        'PASSWORD':'Access',
+    }
+}
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
